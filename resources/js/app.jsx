af6570e9c31dd200b2d1c9eb9401e775695c3a44
Heyqull <a.haiqaal@gmail.com>
2026-06-13 15:22:34 +0800

    Create app.jsx

---
+++ resources/js/app.jsx
@@ -0,0 +1,28 @@
+import './bootstrap';
+import React from 'react';
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
+
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PostsPage from './pages/PostsPage';
+import PostDetailPage from './pages/PostDetailPage';
+import CreatePostPage from './pages/CreatePostPage';
+import EditPostPage from './pages/EditPostPage';
+
+const pages = {
+    LoginPage,
+    RegisterPage,
+    PostsPage,
+    PostDetailPage,
+    CreatePostPage,
+    EditPostPage
+};
+
+createInertiaApp({
+    resolve: name => pages[name],
+    
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
