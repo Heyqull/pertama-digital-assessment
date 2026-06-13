502ad2c4712116e20ede99393e1d98b1e193e30e
Heyqull <a.haiqaal@gmail.com>
2026-06-13 13:22:52 +0800

    base n packages

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pertama Digital Blog</title>
+    <meta name="description" content="A simple blogging platform built with Laravel and React.">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
